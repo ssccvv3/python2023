f59c1165fbd12163afabf8b67c34b0af5f81452b
ssccvv3 <119987000+ssccvv3@users.noreply.github.com>
2023-08-21 22:55:32 +0900

    nested loop/ tuple exercise
    중첩반복문과 튜플 문제풀이

--- loop exercise 10.py
+++ loop exercise 10.py
@@ -6,13 +6,15 @@
 
 filelist = ['exercise01.docx', 'exercise02.docx', 'exercise03.docx', 'exercise04.docx', 'exercise05.docx']
 
-# 내가 푼 문제
-for i in filelist: # list를 문자열로 변경 
-    endd = i.split(".") # 문자열 상태에서 split로 확장명과 이름을 분리
-    del endd[1]   # 삭제로 인덱스 1번인 확장명 삭제
-    print(endd) # 출력을 했으나 괄호가 있는 상태가됨. 인덱스로 나눠진 값이라, 값만 얻을려면 [0] 해야함, 두번 일함
+# # 내가 푼 문제
+# for i in filelist: # list를 문자열로 변경 
+#     endd = i.split(".") # 문자열 상태에서 split로 확장명과 이름을 분리
+#     del endd[1]   # 삭제로 인덱스 1번인 확장명 삭제
+#     print(endd) # 출력을 했으나 괄호가 있는 상태가됨. 인덱스로 나눠진 값이라, 값만 얻을려면 [0] 해야함, 두번 일함
     
 # # 강의 정답
-# for filename in filelist:
-#     filelist_item = filename.split(".")
-#     print (filelist_item[0]) # 삭제가 없이 해당 인덱스의 값만 출력, 괄호가 없음
+for filename in filelist:
+    print(filename)
+    filelist_item = filename.split(".")
+    # print (filelist_item[0]) # 삭제가 없이 해당 인덱스의 값만 출력, 괄호가 없음
+    
--- loop exercise 8.py
+++ loop exercise 8.py
@@ -4,4 +4,4 @@
 # 리스트 변수가 나오니 , for 문으로 하나씩 떼면 됨 
 list_data = ["fun-coding", "Dave", "Linux", "Python", "javascript", "front-end", "back-end", "dataengineering"]
 for i in list_data:
-    print(len(i)) # 데이터 길이 숫자로 각 1줄에 나옴   
+    print(len(i)) # 데이터 길이 숫자로 각 1줄에 나옴    
